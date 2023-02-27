353b39bb9e2901a3f92865779fd62203f024c506
arvind <developer.arvind003@gmail.com>
2023-02-27 18:20:43 +0530

    added error file

--- Backend/app.js
+++ Backend/app.js
@@ -2,8 +2,8 @@ import express from 'express';
 import productRoute from './routes/product.js';
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 //routes import
-app.use("/api/v1",productRoute)
+app.use('/api/v1', productRoute);
 export default app;
