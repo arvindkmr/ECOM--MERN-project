696e0b4c1778f2da15ee1c37bf8865edd904c1f9
arvind <developer.arvind003@gmail.com>
2023-03-14 15:02:55 +0530

    bootstrap added

--- Backend/models/TestingModel.js
+++ Backend/models/TestingModel.js
@@ -58,7 +58,7 @@ const orderSchema = new mongoose.Schema({
     status: {
       type: String,
       //       required: true,
-      // default: "",
+      default: "succeeded",
     },
   },
   user: {
